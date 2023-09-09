4047fb79c14eadfe64739e2e4252a7575cad7393
itzaur <preneur85@gmail.com>
2023-09-09 23:53:02 +0300

    Add Digits explosion

---
+++ Digits explosion.js
@@ -0,0 +1,11 @@
+/*
+DESCRIPTION:
+Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+"312" should return "333122"
+"102269" should return "12222666666999999999"
+*/
+function explode(s) {
+  return [...s].map((el) => Array.from({ length: el }).fill(el)).flat().join``;
+}
