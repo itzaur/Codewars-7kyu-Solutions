071da81a4a7077815f9c43f4bfbcbfcf6f659fc9
itzaur <preneur85@gmail.com>
2023-09-13 17:53:05 +0300

    Add Regex validate PIN code

---
+++ Regex validate PIN code.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+
+Examples (Input --> Output)
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+*/
+function validatePIN(pin) {
+  const numbers = pin.replace(/[0-9]/g, '').length;
+
+  if (pin.length === 4 || pin.length === 6) {
+    return numbers === 0;
+  } else {
+    return false;
+  }
+
+  // return /^(\d{4}|\d{6})$/.test(pin)
+}
