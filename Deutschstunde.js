3c94730caac89f5a60e53de8aa1a3414ff2f02ad
itzaur <preneur85@gmail.com>
2023-10-26 23:43:34 +0300

    Add Deutschstunde

---
+++ Deutschstunde.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+Everybody knows a little german, right? But remembering the correct articles is a tough job. Write yourself a little helper, that returns the noun with the matching article:
+
+each noun containing less than 2 vowels has the article "das"
+each noun containing 2/3 vowels has the article "die"
+each noun containing more than 3 vowels has the article "der"
+Caution: Vowels are "a,e,i,o,u". Umlaute (ä ö ü) are also being counted!
+
+(This Kata is a joke, there is no such grammar rule!)
+*/
+function derDieDas(wort) {
+  const match = wort.match(/[a e i o u ä ö ü]/gi);
+
+  return match == null
+    ? `das ${wort}`
+    : match.length > 3
+    ? `der ${wort}`
+    : match.length >= 2
+    ? `die ${wort}`
+    : match.length > 0
+    ? `das ${wort}`
+    : null;
+}
