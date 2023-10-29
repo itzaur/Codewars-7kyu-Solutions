9e0aacef772d6db91eb3b25512dacdf1f163090b
itzaur <preneur85@gmail.com>
2023-10-29 19:34:12 +0300

    Add Singleton Pattern

---
+++ Singleton Pattern.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+In software engineering, the singleton pattern is a design pattern that restricts the instantiation of a class to one object. This is useful when exactly one object is needed to coordinate actions across the system.
+
+Create an Singleton pattern, so there is one object in system.
+
+Example:
+
+var obj1 = new Singleton();
+var obj2 = new Singleton();
+obj1 === obj2; // => true
+obj1.test = 1;
+obj2.test; // => 1
+*/
+let instance = null;
+
+class Singleton {
+  constructor() {
+    if (instance) return instance;
+    instance = this;
+  }
+}
+
+// class Singleton {
+//   constructor() {
+//     return (Singleton.inst = Singleton.inst ? Singleton.inst : this);
+//   }
+// }
