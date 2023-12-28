03d84b583b2ea01afdb6f1bdbb83662e125d9eb7
itzaur <preneur85@gmail.com>
2023-12-28 23:12:25 +0300

    Add String Reordering

---
+++ String Reordering.js
@@ -0,0 +1,35 @@
+/*
+DESCRIPTION:
+The input will be an array of dictionaries.
+
+Return the values as a string-seperated sentence in the order of their keys' integer equivalent (increasing order).
+
+The keys are not reoccurring and their range is -999 < key < 999. The dictionaries' keys & values will always be strings and will always not be empty.
+
+Example
+Input:
+List = [
+        {'4': 'dog' }, {'2': 'took'}, {'3': 'his'},
+        {'-2': 'Vatsan'}, {'5': 'for'}, {'6': 'a'}, {'12': 'spin'}
+       ]
+
+Output:
+'Vatsan took his dog for a spin'
+*/
+function sentence(List) {
+  const result = List.sort((a, b) => Object.keys(a) - Object.keys(b));
+
+  return result.map((el) => Object.values(el)).join` `;
+}
+
+let List = [
+  { 4: 'dog' },
+  { 2: 'took' },
+  { 3: 'his' },
+  { '-2': 'Vatsan' },
+  { 5: 'for' },
+  { 6: 'a' },
+  { 12: 'spin' },
+];
+
+console.log(sentence(List)); //'Vatsan took his dog for a spin'
