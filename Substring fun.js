507f1629c1c6d7947092223bf331e477eae43b62
itzaur <preneur85@gmail.com>
2024-01-31 01:01:16 +0300

    Add Substring fun

---
+++ Substring fun.js
@@ -0,0 +1,19 @@
+/*
+DESCRIPTION:
+Complete the function that takes an array of words.
+
+You must concatenate the nth letter from each word to construct a new word which should be returned as a string, where n is the position of the word in the list.
+
+For example:
+
+["yoda", "best", "has"]  -->  "yes"
+  ^        ^        ^
+  n=0     n=1     n=2
+Note: Test cases contain valid input only - i.e. a string array or an empty array; and each word will have enough letters.
+*/
+const nthChar = (words) => words.map((el, i) => el[i]).join``;
+
+// const nthChar = words => words.reduce((a, b, index) => a + b[index], "");
+
+console.log(nthChar([])); //''
+console.log(nthChar(['yoda', 'best', 'has'])); //'yes'
