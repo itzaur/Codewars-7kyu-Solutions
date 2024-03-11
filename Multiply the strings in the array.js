eacc6fa8e58c04ab11913a6b02092b205307e509
itzaur <preneur85@gmail.com>
2024-03-11 16:53:25 +0300

    Add Multiply the strings in the array

---
+++ Multiply the strings in the array.js
@@ -0,0 +1,11 @@
+/*
+DESCRIPTION:
+You received an array with two strings. Create a function that will return their product as a string. E.g.
+
+arrMultiply(['9','6']) should return '54'
+*/
+const arrMultiply = ([a, b]) => `${a * b}`;
+
+console.log(arrMultiply(['4', '5'])); //'20'
+console.log(arrMultiply(['2', '-5'])); //'-10'
+console.log(arrMultiply(['9', '0'])); //'0'
