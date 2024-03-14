ebf1e970f4e193f73db2b5f978617d342900c640
itzaur <preneur85@gmail.com>
2024-03-14 14:03:49 +0300

    Add Possibilities Array

---
+++ Possibilities Array.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+A non-empty array a of length n is called an array of all possibilities if it contains all numbers between 0 and a.length - 1 (both inclusive).
+
+Write a function that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+
+Examples:
+
+[1,2,0,3] => True
+# Includes all numbers between 0 and a.length - 1 (4 - 1 = 3)
+
+[0,1,2,2,3] => False
+# Doesn't include all numbers between 0 and a.length - 1 (5 - 1 = 4)
+
+[0] => True
+# Includes all numbers between 0 and a.length - 1 (1 - 1 = 0).
+*/
+function isAllPossibilities(x) {
+  const result = Array.from({ length: x.length }, (_, i) => i);
+  return x.sort((a, b) => a - b).every((el, i) => result[i] == el);
+  ////////////!SECTION
+  //   return !!x.length && x.every((_, idx) => x.includes(idx));
+}
+
+console.log(isAllPossibilities([0, 1, 2, 3])); //true
+console.log(isAllPossibilities([1, 2, 3, 4])); //false
