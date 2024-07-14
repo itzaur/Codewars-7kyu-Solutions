c0db74f31518a0784ac0eb4dba5de9dfdd8de265
itzaur <preneur85@gmail.com>
2024-07-14 20:03:44 +0300

    Add Figurate Numbers #2 - Pronic Number

---
+++ Figurate Numbers #2 - Pronic Number.js
@@ -0,0 +1,51 @@
+/*
+DESCRIPTION:
+Task:
+You have to create a function isPronic to check whether the argument passed is a Pronic Number and return true if it is & false otherwise.
+
+Description:
+Pronic Number -A pronic number, oblong number, rectangular number or heteromecic number, is a number which is the product of two consecutive integers, that is, n(n + 1).
+
+The first few Pronic Numbers are - 0, 2, 6, 12, 20, 30, 42...
+
+Explanation:
+  0 = 0 × 1   // ∴  0 is a Pronic Number
+  2 = 1 × 2   // ∴  2 is a Pronic Number
+  6 = 2 × 3   // ∴  6 is a Pronic Number
+ 12 = 3 × 4   // ∴ 12 is a Pronic Number
+ 20 = 4 × 5   // ∴ 20 is a Pronic Number
+ 30 = 5 × 6   // ∴ 30 is a Pronic Number
+ 42 = 6 × 7   // ∴ 42 is a Pronic Number
+Note:
+Negative numbers are tested as well.
+*/
+function isPronic(n) {
+  const discriminant = 1 - 4 * (n > 0 ? -n : n);
+  const n1 = (-1 + Math.sqrt(discriminant)) / 2;
+  const n2 = (-1 - Math.sqrt(discriminant)) / 2;
+
+  return (
+    Math.abs(Math.abs(n1) - Math.abs(n2)) === 1 &&
+    Number.isInteger(Math.sqrt(discriminant))
+  );
+  ////////////////////////////!SECTION
+  //   return Math.sqrt(1 + 4 * n) % 1 === 0;
+  ////////////////////////////!SECTION
+  //   for (var i = 0; i <= n; i++) {
+  //     if (i * (i + 1) === n) return true;
+  //   }
+
+  //   return false;
+  ////////////////////////////!SECTION
+  //   return Number.isInteger((4 * n + 1) ** 0.5);
+}
+
+console.log(isPronic(0)); //true
+console.log(isPronic(1)); //false
+console.log(isPronic(2)); //true
+console.log(isPronic(3)); //false
+console.log(isPronic(4)); //false
+console.log(isPronic(5)); //false
+console.log(isPronic(6)); //true
+console.log(isPronic(-3)); //false
+console.log(isPronic(-27)); //false
