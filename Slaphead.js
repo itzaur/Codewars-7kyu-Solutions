1c9c8aabdb5b7ee3242dc90e658d883899dc3a3d
itzaur <preneur85@gmail.com>
2024-11-03 23:42:01 +0300

    Add Slaphead

---
+++ Slaphead.js
@@ -0,0 +1,44 @@
+/*
+Description:
+Being a bald man myself, I know the feeling of needing to keep it clean shaven. Nothing worse that a stray hair waving in the wind.
+
+You will be given a string(x). Clean shaved head is shown as "-" and stray hairs are shown as "/". Your task is to check the head for stray hairs and get rid of them.
+
+You should return the original string, but with any stray hairs removed. Keep count ot them though, as there is a second element you need to return:
+
+0 hairs --> "Clean!"
+1 hair --> "Unicorn!"
+2 hairs --> "Homer!"
+3-5 hairs --> "Careless!"
+>5 hairs --> "Hobo!"
+
+So for this head: "------/------" you shoud return:
+
+["-------------", "Unicorn"]
+*/
+function bald(x) {
+  const options = {
+    0: 'Clean!',
+    1: 'Unicorn!',
+    2: 'Homer!',
+    3: 'Careless!',
+    4: 'Careless!',
+    5: 'Careless!',
+  };
+
+  const length = (x.match(/[/]/g) || []).length;
+  const result = x.replace(/[/]/g, '-');
+
+  return [result, options[length] || 'Hobo!'];
+  //////////////////////////////////////////!SECTION
+  //   return [
+  //     '-'.repeat(x.length),
+  //     (['Clean', 'Unicorn', 'Homer', 'Careless', 'Careless', 'Careless'][
+  //       x.split('/').length - 1
+  //     ] || 'Hobo') + '!',
+  //   ];
+}
+
+console.log(bald('/---------')); //['----------', 'Unicorn!']
+console.log(bald('/-----/-')); //['--------', 'Homer!']
+console.log(bald('--/--/---/-/---')); //['---------------', 'Careless!']
