facc0b7aa4f269e1da7b26f80075c7c95f76d1d9
itzaur <preneur85@gmail.com>
2024-11-18 14:00:39 +0300

    Add Difference between years. (Level 1)

---
+++ Difference between years. (Level 1).js
@@ -0,0 +1,20 @@
+/*
+Description:
+Write a function that receives two strings as parameter. This strings are in the following format of date: YYYY/MM/DD. Your job is: Take the years and calculate the difference between them.
+
+Examples:
+'1997/10/10' and '2015/10/10' -> 2015 - 1997 = returns 18
+'2015/10/10' and '1997/10/10' -> 2015 - 1997 = returns 18
+At this level, you don't need validate months and days to calculate the difference.
+*/
+const howManyYears = (date1, date2) =>
+  Math.abs(date1.split('/')[0] - date2.split('/')[0]);
+////////////////////////////////////!SECTION
+// const howManyYears = (date1, date2) =>
+//   Math.abs(parseInt(date1) - parseInt(date2));
+
+console.log(howManyYears('1997/10/10', '2015/10/10')); //18
+console.log(howManyYears('1990/10/10', '2015/10/10')); //25
+console.log(howManyYears('2015/10/10', '1990/10/10')); //25
+console.log(howManyYears('1992/10/24', '2015/10/24')); //23
+console.log(howManyYears('2018/10/10', '2000/10/10')); //18
