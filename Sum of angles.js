eab8db67d3eca1f302e9972db2f4045c0d0a1a06
itzaur <preneur85@gmail.com>
2024-11-30 13:48:48 +0300

    Add Sum of angles

---
+++ Sum of angles.js
@@ -0,0 +1,8 @@
+/*
+Description:
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+*/
+const angle = (n) => 180 * (n - 2);
+
+console.log(angle(3)); //180
+console.log(angle(4)); //360
