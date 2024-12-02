097d0a2b98172a538c5829b19bfe15200bf90340
itzaur <preneur85@gmail.com>
2024-12-02 14:20:01 +0300

    Add Sort arrays - 1

---
+++ Sort arrays - 1.js
@@ -0,0 +1,9 @@
+/*
+Description:
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+*/
+// input: names - unsorted array
+// output: sorted array
+const sortme = (names) => names.sort();
+
+console.log(sortme(['one', 'two', 'three'])); //["one", "three", "two"]
