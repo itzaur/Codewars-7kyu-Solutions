ef9b8917fc727c81f9f84c5be6ba75b4b4dc0c0b
itzaur <preneur85@gmail.com>
2024-12-03 22:24:29 +0300

    Add Odd or Even?

---
+++ Odd or Even?.js
@@ -0,0 +1,41 @@
+/*
+Description:
+Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+Have fun!
+*/
+function oddOrEven(array) {
+  return array.reduce((acc, el) => acc + el, 0) & (1 === 1) ? 'odd' : 'even';
+  //////////////////////////////////!SECTION
+  //   return array.reduce((a, b) => a ^ b, 0) & 1 ? 'odd' : 'even';
+}
+
+console.log(oddOrEven([0])); //'even'
+console.log(oddOrEven([1])); //'odd'
+console.log(oddOrEven([])); //'even'
+console.log(oddOrEven([0, 1, 5])); //'even'
+console.log(oddOrEven([0, 1, 3])); //'even'
+console.log(oddOrEven([1023, 1, 2])); //'even'
+console.log(oddOrEven([0, -1, -5])); //'even'
+console.log(oddOrEven([0, -1, -3])); //'even'
+console.log(oddOrEven([-1023, 1, -2])); //'even'
+console.log(oddOrEven([0, 1, 2])); //'odd'
+console.log(oddOrEven([0, 1, 4])); //'odd'
+console.log(oddOrEven([1023, 1, 3])); //'odd'
+console.log(oddOrEven([0, -1, 2])); //'odd'
+console.log(oddOrEven([0, 1, -4])); //'odd'
+console.log(oddOrEven([-1023, -1, 3])); //'odd'
