6ae8910031120ba530715181c9f6e4889b4c2682
itzaur <preneur85@gmail.com>
2024-12-10 10:22:36 +0300

    Add Largest Square Inside A Circle

---
+++ Largest Square Inside A Circle.js
@@ -0,0 +1,9 @@
+/*
+Description:
+Determine the area of the largest square that can fit inside a circle with radius r.
+*/
+const areaLargestSquare = (r) => r ** 2 * 2;
+
+console.log(areaLargestSquare(5)); //50
+console.log(areaLargestSquare(7)); //98
+console.log(areaLargestSquare(15)); //450
