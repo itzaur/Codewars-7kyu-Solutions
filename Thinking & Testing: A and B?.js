595c1bdb7dbba742ee01bcb543ec546cb4dcdfc6
itzaur <preneur85@gmail.com>
2024-12-15 14:23:43 +0300

    Add Thinking & Testing: A and B?

---
+++ Thinking & Testing: A and B?.js
@@ -0,0 +1,21 @@
+/*
+Description:
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+*/
+const testit = (a, b) => a | b;
+
+console.log(testit(0, 1)); //1
+console.log(testit(1, 2)); //3
+console.log(testit(10, 20)); //30
+console.log(testit(1, 1)); //1
+console.log(testit(1, 3)); //3
+console.log(testit(12, 71)); //79
+console.log(testit(68, 37)); //101
+console.log(testit(89, 10)); //91
+console.log(testit(42, 3)); //43
