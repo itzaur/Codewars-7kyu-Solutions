a7de3c0480c56f8e20b220ad1a127e5f6e34a000
itzaur <preneur85@gmail.com>
2024-12-23 23:37:08 +0300

    Add JavaScript seems to be broken

---
+++ JavaScript seems to be broken.js
@@ -0,0 +1,14 @@
+/*
+Description:
+There is some horribly rotten script that is not required in your project anymore, and it is affecting the way your code is supposed to work.
+
+For some reason you don't have an access to that rotten piece of code but you want to get your code working as expected in any possible way.
+
+Go and get it working!
+*/
+function semicolonSeparationToCommaSeparation(input) {
+  // meaningless kata
+  return input.slice().split`;`._join`,`;
+  //////////////////////////////////!SECTION
+  //   return input.split(';').toLocaleString();
+}
