91d218e11c615d90a23462919fdc9a0130fbb662
itzaur <preneur85@gmail.com>
2024-12-24 15:11:10 +0300

    Add Static privacy

---
+++ Static privacy.js
@@ -0,0 +1,43 @@
+/*
+Description:
+I wrote a class Class, with a private static property foo and a getter to retrieve it:
+
+class Class {
+  static #foo = 42;
+
+  static get foo() {
+    return this.#foo;
+  }
+}
+Class.foo; // 42
+However, when I create a class Subclass that inherits from Class, the code breaks:
+
+class Subclass extends Class {}
+Subclass.foo; // Error !
+Can you fix the code ?
+
+Modify Class so that Subclass.foo will work correctly. This should also work for any level of inheritance from Class, e.g. with:
+
+class Deepclass extends Subclass {}
+Deepclass.foo; // should return 42
+*/
+class Class {
+  static #foo = 42;
+
+  static get foo() {
+    return Class.#foo;
+  }
+}
+
+class Subclass extends Class {}
+class Deepclass extends Subclass {}
+
+console.log(Class.foo); //42
+console.log(Subclass.foo); //42
+console.log(Deepclass.foo); //42
+
+Class.foo = Subclass.foo = Deepclass.foo = 666;
+
+console.log(Class.foo); //42
+console.log(Subclass.foo); //42
+console.log(Deepclass.foo); //42
