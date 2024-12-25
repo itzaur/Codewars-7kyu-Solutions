02538cdd95e2c651282d0b7ec566681e8a95337b
itzaur <preneur85@gmail.com>
2024-12-25 22:21:21 +0300

    Add Debug Basic Calculator

---
+++ Debug Basic Calculator.js
@@ -0,0 +1,38 @@
+/*
+Description:
+Debug a function called calculate that takes 3 values. The first and third values are numbers. The second value is a character. If the character is "+" , "-", "*", or "/", the function will return the result of the corresponding mathematical function on the two numbers. If the string is not one of the specified characters, the function should return null.
+
+calculate(2,"+", 4); //Should return 6
+calculate(6,"-", 1.5); //Should return 4.5
+calculate(-4,"*", 8); //Should return -32
+calculate(49,"/", -7); //Should return -7
+calculate(8,"m", 2); //Should return null
+calculate(4,"/",0) //should return null
+Kind of a fork (not steal :)) of Basic Calculator kata by TheDoctor.
+*/
+const calculate = (a, o, b) => {
+  const operations = {
+    '+': a + b,
+    '-': a - b,
+    '*': a * b,
+    '/': b !== 0 ? a / b : null,
+  };
+
+  return operations[o] ?? null;
+  ////////////////////////////////////!SECTION
+  //   const ops = {
+  //     '+': (a, b) => a + b,
+  //     '-': (a, b) => a - b,
+  //     '*': (a, b) => a * b,
+  //     '/': (a, b) => (b ? a / b : null),
+  //   };
+
+  //   return op in ops ? ops[op](a, b) : null;
+};
+
+console.log(calculate(2, '+', 4)); //6
+console.log(calculate(6, '-', 1.5)); //4.5
+console.log(calculate(-4, '*', 8)); //-32
+console.log(calculate(49, '/', -7)); //-7
+console.log(calculate(8, 'm', 2)); //null
+console.log(calculate(4, '/', 0)); //null
