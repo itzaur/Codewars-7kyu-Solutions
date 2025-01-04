e3364b48dc80cc0885776d4941c1197776ecc6c8
itzaur <preneur85@gmail.com>
2025-01-04 22:12:33 +0300

    Add Oh come on! Hello world! once again?

---
+++ Oh come on! Hello world! once again?.js
@@ -0,0 +1,13 @@
+/*
+Description:
+Task
+Write a function f (in C# F) that returns Hello, world! string. But there is one thing, you cannot use helowrd letters.
+
+The C# test cases ignore checking for banned letters in words class, string and public.
+The C++ test cases ignore checking for banned letters in words string, std and return.
+
+Have fun!
+*/
+f = (_) => '\x48\x65\x6C\x6C\x6F\x2C\x20\x77\x6F\x72\x6C\x64\x21';
+
+console.log(f()); //"Hello, world!"
