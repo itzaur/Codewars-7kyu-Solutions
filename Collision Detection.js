eca85a144a60af84d8c954713f1260ecd4935637
itzaur <preneur85@gmail.com>
2025-01-27 23:55:19 +0300

    Add Collision Detection

---
+++ Collision Detection.js
@@ -0,0 +1,23 @@
+/*
+Description:
+Create a function to determine whether or not two circles are colliding. You will be given the position of both circles in addition to their radii:
+
+function collision(x1, y1, radius1, x2, y2, radius2) {
+  // collision?
+}
+If a collision is detected, return true. If not, return false.
+
+Here's a geometric diagram of what the circles passed in represent:
+*/
+function collision(x1, y1, radius1, x2, y2, radius2) {
+  return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) <= radius1 + radius2;
+  //////////////////////////////////////////!SECTION
+  //   return Math.hypot(x1 - x2, y1 - y2) < radius1 + radius2;
+}
+
+console.log(collision(1, 1, 1, 1.1, 1.1, 0.1)); //true
+console.log(collision(-1, 1, 10, -10.1, 1.1, 1)); //true
+console.log(collision(-5, 5, 5.0001, 5, -5, 5 * Math.sqrt(5))); //true
+console.log(collision(1, 1, 0.01, 1, 1.1, 0.01)); //false
+console.log(collision(-1, 1, 6, -10.1, 1.1, 1)); //false
+console.log(collision(-5, 5, 5.0001, 5, -5, 4 * Math.sqrt(5))); //false
