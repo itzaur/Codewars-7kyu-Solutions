03cc147ee411e1d93ee17e6375c4c3e6fb2cf114
itzaur <preneur85@gmail.com>
2025-01-29 21:56:13 +0300

    Add Reverse list

---
+++ Reverse list.js
@@ -0,0 +1,8 @@
+/*
+Description:
+Write reverseList function that simply reverses lists.
+*/
+const reverseList = (arr) => arr.reverse();
+
+console.log(reverseList([])); //[]
+console.log(reverseList([1, 2, 3])); //[3,2,1]
