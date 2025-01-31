49b00560e649c12c32c3a8490aa7d08c1d2e9410
itzaur <preneur85@gmail.com>
2025-01-31 19:15:18 +0300

    Add Get length of the list recursively

---
+++ Get length of the list recursively.js
@@ -0,0 +1,16 @@
+/*
+Description:
+Write funcion lenR which returns the length of a given list. Try no to cheat and provide recursive solution.
+*/
+const lenR = (x) => (x ? x.length : lenR(x + 1));
+////////////////////////////////////////////!SECTION
+// const lenR = x => x.length && 1 + lenR(x.slice(1));
+////////////////////////////////////////////!SECTION
+// function lenR([head, ...tail]) {
+//   return !head ? 0 : lenR(tail) + 1;
+// }
+////////////////////////////////////////////!SECTION
+// const lenR = $ => $.length === 0 ? 0 : 1 + lenR($.slice(1));
+
+console.log(lenR([])); //0
+console.log(lenR([1])); //1
