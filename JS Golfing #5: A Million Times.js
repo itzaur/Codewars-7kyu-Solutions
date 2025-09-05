cec28fa94d77f23e619fb61bacbc000cdde3c6c8
itzaur <preneur85@gmail.com>
2025-09-05 23:37:59 +0300

    Add JS Golfing #5: A Million Times

---
+++ JS Golfing #5: A Million Times.js
@@ -0,0 +1,16 @@
+/*
+Description:
+
+ JS Golfing Series
+
+
+Task:
+
+Return the input number multiplied by a million with no more than 10 characters.
+*/
+x = (n) => n * 1e6;
+
+console.log(x(1)); //1_000_000
+console.log(x(2)); //2_000_000
+console.log(x(3)); //3_000_000
+console.log(x(4)); //4_000_000
