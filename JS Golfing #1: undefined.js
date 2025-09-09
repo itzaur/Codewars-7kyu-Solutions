6dcd7d2bb6c06625ed1cc7609f5683faca9a101e
itzaur <preneur85@gmail.com>
2025-09-09 23:49:14 +0300

    Add JS Golfing #1: undefined

---
+++ JS Golfing #1: undefined.js
@@ -0,0 +1,16 @@
+/*
+Description:
+
+ JS Golfing Series
+
+
+Task:
+
+Return undefined with no more than 10 characters.
+
+    Digits and letters are not allowed!
+    Your function should take exactly `0` parameters.
+*/
+x = () => {};
+
+console.log(x()); //undefined
