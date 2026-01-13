fde058f3f9a2c07bdea3c63874eadb06132bb22a
itzaur <preneur85@gmail.com>
2026-01-13 23:13:45 +0300

    Add ScholarStem: Unit 6- Baby count!

---
+++ ScholarStem: Unit 6- Baby count!.js
@@ -0,0 +1,40 @@
+/*
+Description:
+
+A local birthing center is interested in names!
+
+They have arrays of all the baby names they see each year, but the lists are sooo long! They don’t know how to calculate how many times one name is used.
+
+Given an array of names and a specific name string, return the number of times that specific name appears in the array.
+
+countName( ["Tom","Bob","Harry","Bob"] , "Bob") //should return 2, since "Bob" shows up 2 times in the array
+*/
+function countName(anArr, name) {
+  const occurrences = anArr.reduce(
+    (acc, el) => ({ ...acc, [el]: (acc[el] || 0) + 1 }),
+    {}
+  );
+
+  return occurrences[name] || 0;
+  ////////////////////////////////////!SECTION
+  //   return anArr.filter((x) => x == name).length;
+  ////////////////////////////////////!SECTION
+  //   return anArr.reduce((pre, val) => pre + (val === name), 0);
+}
+
+const MAINLIST = [
+  'Bob',
+  'Ted',
+  'Amy',
+  'Alice',
+  'Bob',
+  'Ted',
+  'Amy',
+  'Ted',
+  'Amy',
+  'Fred',
+];
+
+console.log(countName(MAINLIST, 'Ted')); //3
+console.log(countName(MAINLIST, 'Amy')); //3
+console.log(countName(MAINLIST, 'Bob')); //2
