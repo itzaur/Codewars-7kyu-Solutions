2737408dfeebd9ddc3c8d1ed260e729159b36451
itzaur <preneur85@gmail.com>
2026-01-17 23:39:38 +0300

    Add Date Format Validation

---
+++ Date Format Validation.js
@@ -0,0 +1,16 @@
+/*
+Description:
+
+Create a function that will return true if the input is in the following date time format 01-09-2016 01:20 and false if it is not.
+
+This Kata has been inspired by the Regular Expressions chapter from the book Eloquent JavaScript.
+*/
+function dateChecker(date) {
+  return /^\d{2}-\d{2}-\d{4} \d{2}:\d{2}$/.test(date);
+}
+
+console.log(dateChecker('01-09-2016 01:20')); //true
+console.log(dateChecker('01-09-2016 01;20')); //false
+console.log(dateChecker('01_09_2016 01:20')); //false
+console.log(dateChecker('14-10-1066 12:00')); //true
+console.log(dateChecker('Tenth of January')); //false
