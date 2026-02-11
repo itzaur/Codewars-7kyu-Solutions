e955f42edf58f9b1d0b214e168a72243538126fa
itzaur <preneur85@gmail.com>
2026-02-11 23:05:50 +0300

    Add Geometric Progression Sequence

---
+++ Geometric Progression Sequence.js
@@ -0,0 +1,19 @@
+/*
+Description:
+
+In your class, you have started lessons about geometric progression. Since you are also a programmer, you have decided to write a function that will print first n elements of the sequence with the given constant r and first element a.
+
+Result should be separated by comma and space.
+Example
+
+geometricSequenceElements(2, 3, 5) == '2, 6, 18, 54, 162'
+
+More info: https://en.wikipedia.org/wiki/Geometric_progression
+*/
+function geometricSequenceElements(a, r, n) {
+  return Array.from({ length: n }, (_, i) => a * r ** i).join(', ');
+}
+
+console.log(geometricSequenceElements(2, 3, 5)); //'2, 6, 18, 54, 162'
+console.log(geometricSequenceElements(2, 2, 10)); //'2, 4, 8, 16, 32, 64, 128, 256, 512, 1024'
+console.log(geometricSequenceElements(1, -2, 10)); //'1, -2, 4, -8, 16, -32, 64, -128, 256, -512'
