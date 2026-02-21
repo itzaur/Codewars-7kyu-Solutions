0ffe52e1ddf92420a9ddf3f6f15cc3147bc68c0c
itzaur <preneur85@gmail.com>
2026-02-21 23:02:54 +0300

    Add Consecutive Digits To Form Sum

---
+++ Consecutive Digits To Form Sum.js
@@ -0,0 +1,35 @@
+/*
+Description:
+
+Positive integers have so many gorgeous features. Some of them could be expressed as a sum of two or more consecutive positive numbers.
+Consider an Example :
+
+    10 could be expressed as the sum of 1 + 2 + 3 + 4 .
+    Task
+
+Given Positive integer, N , return true if it could be expressed as a sum of two or more consecutive positive numbers , otherwise return false .
+Notes
+
+    Guaranteed constraint : 2 ≤ N ≤ (2^32) -1 .
+
+Input >> Output Examples:
+
+
+* consecutiveDucks(9)  ==>  return (true)  //  9 , could be expressed as a sum of ( 2 + 3 + 4 ) or ( 4 + 5 ) .
+
+* consecutiveDucks(64)  ==>  return (false)
+
+* consecutiveDucks(42)  ==>  return (true) //  42 , could be expressed as a sum of ( 9 + 10 + 11 + 12 )  .
+*/
+function consecutiveDucks(num) {
+  return (num & (num - 1)) !== 0;
+}
+////////////////////////////////////!SECTION
+// const consecutiveDucks = (num) => !!(num & --num);
+////////////////////////////////////!SECTION
+// const consecutiveDucks = num => Math.log2(num) % 1 !== 0
+
+console.log(consecutiveDucks(69)); //true
+console.log(consecutiveDucks(8)); //false
+console.log(consecutiveDucks(57)); //true
+console.log(consecutiveDucks(6)); //true
