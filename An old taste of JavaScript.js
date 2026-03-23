d9f6e28300a99ffc76d9f3cec2a9367777f0cf0d
itzaur <preneur85@gmail.com>
2026-03-23 22:54:18 +0300

    Add An old taste of JavaScript

---
+++ An old taste of JavaScript.js
@@ -0,0 +1,16 @@
+/*
+Description:
+
+Back in the days, not all web browsers had a JavaScript engine. Some old browsers did not even recognize the <script> HTML tag and would just display its content, i.e. the JavaScript source code.
+
+Webpages that contained JavaScript needed a way to prevent this. A clever but rather hacky solution was designed to fix this issue. This solution is also that of this kata.
+*/
+/*
+<!--
+  console.log('Hello World')
+-->
+*/
+////////////////////////////////!SECTION
+// -->
+////////////////////////////////!SECTION
+// <!--
