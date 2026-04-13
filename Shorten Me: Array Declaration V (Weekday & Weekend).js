31793c70bc1d7b2c6288ceb31d1198a70dac5132
itzaur <preneur85@gmail.com>
2026-04-13 22:57:49 +0300

    Add Shorten Me: Array Declaration V (Weekday & Weekend)

---
+++ Shorten Me: Array Declaration V (Weekday & Weekend).js
@@ -0,0 +1,18 @@
+/*
+Description:
+Shorten Me: Array Declaration V (Weekday & Weekend)
+Task
+
+You have to write an array() function returning an array object with elements as follow:
+
+[ 'sunday', 'monday', 'tuesday', 'wednesday',
+  'thursday','friday','saturday' ]
+
+Coding Limitation:
+
+Less than 75 characters
+*/
+array = () =>
+  'sun,mon,tues,wednes,thurs,fri,satur'.split`,`.map((t) => t + 'day');
+
+console.log(array()); //['sunday', 'monday', 'tuesday', 'wednesday','thursday','friday','saturday' ]
