1b1580cadc62714cffb85e4cd33aad75732005ab
itzaur <preneur85@gmail.com>
2026-04-17 22:40:19 +0300

    Add Sentence to words

---
+++ Sentence to words.js
@@ -0,0 +1,12 @@
+/*
+Description:
+
+Write function splitSentence which will create a list of strings from a string.
+
+Example:
+
+"hello world" -> ["hello", "world"]
+*/
+const splitSentence = (s) => s.split` `;
+
+console.log(splitSentence('hello world')); //["hello", "world"]
