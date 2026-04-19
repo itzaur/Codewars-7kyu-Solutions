d39a87bb15b3d7a7e6eae97d601a30ce524bfd07
itzaur <preneur85@gmail.com>
2026-04-19 21:51:25 +0300

    Add Enumerable Magic #5- True for Just One?

---
+++ Enumerable Magic #5- True for Just One?.js
@@ -0,0 +1,35 @@
+/*
+Description:
+Task
+
+Create a function called one that accepts two params:
+
+    a sequence
+    a function
+
+and returns true only if the function in the params returns true for exactly one (1) item in the sequence.
+Example
+
+one([1, 3, 5, 6, 99, 1, 3], bigger_than_ten) -> true
+one([1, 3, 5, 6, 99, 88, 3], bigger_than_ten) -> false
+one([1, 3, 5, 6, 5, 1, 3], bigger_than_ten) -> false
+
+If you need help, here is a resource ( in Ruby ).
+*/
+const one = (arr, fun) => arr.filter(fun).length === 1;
+
+console.log(
+  one([1, 2, 3, 4, 5], function (item) {
+    return item < 2;
+  }),
+); //true
+console.log(
+  one([1, 2, 3, 4, 5], function (item) {
+    return item % 2;
+  }),
+); //false
+console.log(
+  one([1, 2, 3, 4, 5], function (item) {
+    return item > 5;
+  }),
+); //false
