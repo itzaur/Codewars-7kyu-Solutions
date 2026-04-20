9f02ae0740f67ea8c07763fdb67a3aa8ecccad42
itzaur <preneur85@gmail.com>
2026-04-20 23:18:03 +0300

    Add Shorten Me: Array Declaration II (undefined)

---
+++ Shorten Me: Array Declaration II (undefined).js
@@ -0,0 +1,18 @@
+/*
+Description:
+Shorten Me: Array Declaration II (undefined)
+Task
+
+You have to write an array() function returning an array object with elements as follow:
+
+[ 'u', 'n', 'd', 'e', 'f', 'i', 'n', 'e', 'd' ]
+
+Coding Limitation:
+
+Less than 25 characters
+*/
+array = (_) => [...(void 0 + '')];
+//////////////////////////!SECTION
+// array = (_) => [...('' + _)];
+
+console.log(array()); //[ 'u', 'n', 'd', 'e', 'f', 'i', 'n', 'e', 'd' ]
