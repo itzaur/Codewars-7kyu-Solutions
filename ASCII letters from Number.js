8c7f4d24a5eabdaee543d405fe6f477861e148af
itzaur <preneur85@gmail.com>
2026-05-02 21:58:35 +0300

    Add ASCII letters from Number

---
+++ ASCII letters from Number.js
@@ -0,0 +1,29 @@
+/*
+Description:
+
+You have to create a function that converts integer given as string into ASCII uppercase letters or spaces.
+
+All ASCII characters have their numerical order in table.
+
+For example,
+
+from ASCII table, character of number 65 is "A".
+
+Numbers will be next to each other, So you have to split given number to two digit long integers.
+Examples (input -> output)
+
+'658776' -> 'AWL' (because in ASCII table 'A' is 65, 'W' is 87, 'L' is 76)
+'73327673756932858080698267658369' ->'I LIKE UPPERCASE'
+
+Good Luck!
+*/
+function convert(number) {
+  return number.match(/.{1,2}/g).map((el) => String.fromCharCode(el)).join``;
+  ////////////////////////////////////////!SECTION
+  //   return String.fromCharCode(...number.match(/../g))
+}
+
+console.log(convert('65')); //"A"
+console.log(convert('656667')); //"ABC"
+console.log(convert('676584')); //"CAT"
+console.log(convert('73327673756932858080698267658369')); //"I LIKE UPPERCASE"
