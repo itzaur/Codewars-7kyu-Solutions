c9a2f324d40a054bf57cd6b5d815493015b11845
itzaur <preneur85@gmail.com>
2026-05-03 23:12:34 +0300

    Add Convert a Number to money!!

---
+++ Convert a Number to money!!.js
@@ -0,0 +1,29 @@
+/*
+Description:
+Simplified number to money converter.
+Note:
+
+    truncate to 2 decimal places
+    don't keep trailing zeros
+    input will be a positive number with at least two trailing digits
+
+Examples:
+
+2546.2562 --> '2,546.25'
+
+1500.342626 --> '1,500.34'
+
+100.2134 --> '100.21'
+
+Taken from 'Formatting a number as price' (Kyu 5)
+*/
+var numberToMoney = function (n) {
+  return (~~(n * 100) / 100).toLocaleString('en-US');
+};
+//////////////////////////////////////////!SECTION
+// const numberToMoney = (n) => (Math.floor(n * 100) / 100).toLocaleString();
+
+console.log(numberToMoney(2546.2562)); //'2,546.25'
+console.log(numberToMoney(1500.342626)); //'1,500.34'
+console.log(numberToMoney(100.2134)); //'100.21'
+console.log(numberToMoney(2342.01)); //'2,342.01'
