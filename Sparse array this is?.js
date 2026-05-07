9d0f7772a123e64747b42d0e5934b0839edbab1d
itzaur <preneur85@gmail.com>
2026-05-07 22:30:05 +0300

    Add Sparse array this is?

---
+++ Sparse array this is?.js
@@ -0,0 +1,21 @@
+/*
+Description:
+
+JavaScript has sparse arrays, in which not all of its elements are necessarily set. As a result, the length of a javascript array may be greater than the number of its elements. It would be useful to have an array method that checks whether or not an array is sparse.
+
+The challenge here is to create such a method. Please remember that an element in an array can be set to anything including "null" and "undefined". Such elements are not considered sparse in JavaScript. One of the ways to make sparse arrays is for example by deleting some elements in a regular array, like in "delete array[i]", where i < array.length. Also remember that an empty array is not a sparse array by definition.
+*/
+Array.prototype.isSparse = function () {
+  return this.length > Object.keys(this).length;
+  //////////////////////////////////////!SECTION
+  //   return this.length != this.filter((el) => el == el).length;
+  //////////////////////////////////////!SECTION
+  //   return this.reduce(sum => ++sum , 0) < this.length
+};
+
+var array1 = [1, 2, 3],
+  array2 = [, 2, 3],
+  msg1 = '[' + array1.toString() + '] is not a sparse array!',
+  msg2 = '[' + array2.toString() + '] is a sparse array';
+console.log(!array1.isSparse()); //msg1
+console.log(array2.isSparse()); //msg2
