8671086307e871047411555eb7a2e3da763adb1a
itzaur <preneur85@gmail.com>
2026-06-02 23:51:54 +0300

    Add Functional Addition

---
+++ Functional Addition.js
@@ -0,0 +1,18 @@
+/*
+Description:
+
+Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+var addOne = add(1);
+addOne(3); // 4
+
+var addThree = add(3);
+addThree(3); // 6
+*/
+const add = (n) => (el) => n + el;
+
+console.log(add(1)(3)); //4
+console.log(add(1)(-5)); //-4
+console.log(add(3)(20)); //23
