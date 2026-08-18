03091e719f101cb43ac44c447cf3fd3fa2d9353e
itzaur <preneur85@gmail.com>
2026-08-18 23:13:09 +0300

    Add Truthy and Falsy

---
+++ Truthy and Falsy.js
@@ -0,0 +1,21 @@
+/*
+Description:
+
+In JavaScript there are truthy and falsy values:
+
+    truthy: a value that when evaluated in a boolean context returns true
+    falsy: a value that when evaluated in a boolean context returns false
+
+MDN links:
+
+    https://developer.mozilla.org/en/docs/Glossary/Truthy
+    https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+
+Your task
+
+You are given two empty arrays (truthy and falsy) and you have to fill this array with at least 5 elements in each which will evaluate to true or false accordingly.
+*/
+const truthy = [69, '69', [69], {}, true];
+const falsy = ['', 0, undefined, null, false];
+
+console.log(new Set(truthy).size); //5
